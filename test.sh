6578acb79bc5830cbe9ca872e538f85d0b2aa479
root <root@fnaranjo.ec.ibm.com>
2022-01-21 22:28:31 -0500

    first commit

---
+++ test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+testlock(){
+
+#./csmcli.sh cmdsess -quiet -action suspend ${1}
+
+
+mensaje=800;
+
+echo $mensaje' '$1;
+
+
+}
+
---
+++ test2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+testlock(){
+
+#./csmcli.sh cmdsess -quiet -action recover ${GSESSION}
+
+mensaje=600;
+
+echo $mensaje' '$1;
+
+
+}
+
